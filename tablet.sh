aac15b59ddaf1e39a432b696694b03682cb0a19a
mateuszpiorowski <mateuszpiorowski@gmail.com>
2022-11-23 20:32:14 +0100

    update

--- tablet.sh
+++ tablet.sh
@@ -1,7 +1,7 @@
 adb reverse tcp:5900 tcp:5900
 
-xrandr --newmode "720x1560_60.00"  96.07  720 776 856 992  1560 1561 1564 1614  -HSync +Vsync
-xrandr --addmode HDMI-0 720x1560_60.00
+xrandr --newmode "1560x720_59.90"  91.16  1560 1640 1800 2040  720 721 724 746  -HSync +Vsync
+xrandr --addmode HDMI-0 1560x720_59.90
 
-xrandr --output HDMI-0 --mode 720x1560_60.00 --right-of DP-4
-x11vnc -clip 720x1560+0+0
+xrandr --output HDMI-0 --mode 1560x720_59.90 --auto --right-of DP-4
+x11vnc -clip 1560x720+0+0 -scale 0.5 -forever
