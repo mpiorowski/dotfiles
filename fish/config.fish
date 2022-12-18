582da3bacff103ec9987171f45bb13d61bef8a07
mateuszpiorowski <mateuszpiorowski@gmail.com>
2022-12-18 22:45:08 +0100

    update

--- fish/config.fish
+++ fish/config.fish
@@ -1,5 +1,5 @@
 if status is-interactive
-  # set BROWSER microsoft-edge-dev
+  set BROWSER microsoft-edge-dev
   # export PATH="$PATH:$(go env GOPATH)/bin"
     # Commands to run in interactive sessions can go here
 end
