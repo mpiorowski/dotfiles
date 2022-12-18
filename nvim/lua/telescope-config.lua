c1419d49df5e72af2d4cf2d225c56aa61fca06e9
mateuszpiorowski <mateuszpiorowski@gmail.com>
2022-12-18 22:32:10 +0100

    update

--- nvim/lua/telescope-config.lua
+++ nvim/lua/telescope-config.lua
@@ -3,42 +3,47 @@ local telescope = require 'telescope'
 local actions = require 'telescope.actions'
 local fb_actions = telescope.extensions.file_browser.actions  
 function telescope_buffer_dir()
-  return vim.fn.expand('%:p:h')
+    return vim.fn.expand('%:p:h')
 end
 telescope.setup{
-  defaults = {
-    file_ignore_patterns = {
-      ".git/",
-      "node_modules"
+    defaults = {
+        file_ignore_patterns = {
+            ".git/",
+            "node_modules"
+        },
+        hijack_netrw = true,
+        mappings = {
+            n = {
+                ['q'] = actions.close,
+                ["<C-cr>"] = actions.send_selected_to_qflist,
+            }
+        },
     },
-    hijack_netrw = true,
-    hidden = true,
-    mappings = {
-      n = {
-        ['q'] = actions.close,
-        ["<C-cr>"] = actions.send_selected_to_qflist,
-      }
-    },
-  },
-  extensions = {
-    file_browser = {
-      hijack_netrw = true,
-      mappings = {
-         ['i'] = {
-           ['<C-w>'] = function() vim.cmd('normal vbd') end,
-         },
-         ['n'] = {
-           ['N'] = fb_actions.create,
-           ['R'] = fb_actions.rename,
-           ['D'] = fb_actions.remove,
-           ['h'] = fb_actions.goto_parent_dir,
-           ['/'] = function()
-             vim.cmd('startinsert')
-           end
-         }
-      }
+    extensions = {
+        file_browser = {
+            hijack_netrw = true,
+            hidden = true,
+            no_ignore = true,
+            initial_mode = "normal",
+            all_previewer = true,
+            path = "%:p:h",
+            cwd = telescope_buffer_dir(),
+            mappings = {
+                ['i'] = {
+                    ['<C-w>'] = function() vim.cmd('normal vbd') end,
+                },
+                ['n'] = {
+                    ['N'] = fb_actions.create,
+                    ['R'] = fb_actions.rename,
+                    ['D'] = fb_actions.remove,
+                    ['h'] = fb_actions.goto_parent_dir,
+                    ['/'] = function()
+                        vim.cmd('startinsert')
+                    end
+                }
+            }
+        }
     }
-  }
 }
 telescope.load_extension("file_browser")
 local opts = { noremap = true, silent = true }
@@ -47,6 +52,6 @@ vim.keymap.set('n', 'gf', '<cmd>lua require("telescope.builtin").git_files()<cr>
 vim.keymap.set('n', 'gt', '<cmd>lua require("telescope.builtin").lsp_type_definitions()<cr>', opts)
 vim.keymap.set('n', 'gr', '<cmd>lua require("telescope.builtin").lsp_references()<cr>', opts)
 vim.keymap.set('n', 'gv', '<cmd>lua require("telescope.builtin").live_grep({ hidden = true })<cr>', opts)
-vim.keymap.set('n', 'gb', '<cmd>lua require("telescope").extensions.file_browser.file_browser({ path = "%:p:h", cwd = telescope_buffer_dir(), hidden = true, grouped = true, all_previewer = true, initial_mode = "normal" })<cr>', opts)
+vim.keymap.set('n', 'gb', '<cmd>lua require("telescope").extensions.file_browser.file_browser()<cr>', opts)
 vim.keymap.set('n', 'gy', '<cmd>lua require("telescope.builtin").buffers({initial_mode = "normal"})<cr>', opts)
 vim.keymap.set('n', 'gh', '<cmd>lua require("telescope.builtin").lsp_definitions({initial_mode = "normal"})<cr>', opts)
